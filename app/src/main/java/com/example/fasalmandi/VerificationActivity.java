7f8c17229a1fc50ea4979c3c8b8d884b2d7eda18
sobhu <subhashishanand22@gmail.com>
2021-01-08 17:20:47 +0530

    ProfileActivity code added

--- app/src/main/java/com/example/fasalmandi/VerificationActivity.java
+++ app/src/main/java/com/example/fasalmandi/VerificationActivity.java
@@ -6,9 +6,12 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VerificationActivity extends AppCompatActivity {
     private HomeFragment homeFrag;
+    Button accountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,14 @@ public class VerificationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_verification);
         homeFrag=new HomeFragment();
         setFragment(homeFrag);
+        accountButton=findViewById(R.id.accountcheckButton);
+
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(VerificationActivity.this, ProfileActivity.class));
+            }
+        });
 
     }
     //Meathod for setting the fragment in this layout
